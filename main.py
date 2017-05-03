69c71f071fb38f7637de30345c10b53d2d0650aa
CYUlysses <ulysses.cy@gmail.com>
2017-05-03 18:42:38 -0400

    updated

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-from senti_scoring import *
-import pickle
+# from senti_scoring import *
+import shutil
 
-os.chdir('redata')
+datafolder = 'data_nlp'
+validfiles = pd.read_csv('valid_file.csv')
+filelist = validfiles['file_name']
 
-filedict = {}
-for fileind, onefile in enumerate(os.listdir()):
-    if fileind >= 5:
-        break
+#os.chdir('redata')
+os.chdir(datafolder)
+#filelist = os.listdir()
+attrlist = ['Tic', 'Date','Type'] + LM_Selected + Harvard_Selected
+entries = np.array(np.zeros((1,len(attrlist))))
+
+for fileind, onefile in enumerate(filelist):
+    print('Processing file #%s, file name "%s"' % (str(fileind), onefile))
     alltxt = read_bulk(onefile)
     testlist = get_candidates(alltxt)
     docscore = find_scores(testlist)
-    filedict[onefile[0:-4]] = docscore
-
-with open('somedata.pickle','wb') as handle:
-    pickle.dump(filedict, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
+    ids = np.array([onefile[:-4].split('-')])
+    docentry = np.concatenate((ids, docscore), axis=1)
+    entries = np.append(entries, docentry, axis=0)
+    if fileind % 10 == 5:
+        shutil.copy2('tempsave.csv','tempreadable.csv')
+        tblsave = pd.DataFrame(entries[1:,:], columns=attrlist)
+        tblsave.to_csv('tempsave.csv', sep=',',
+                      encoding='utf-8', mode='w')
 os.chdir('..')
+tblout = pd.DataFrame(entries, columns=attrlist)
+tblout.to_csv('NLPed.csv', sep=',', encoding='utf-8', mode='w')
+
+print('The end is the beginning is the end')
--- senti_scoring.py
+++ senti_scoring.py
@@ -12,29 +12,32 @@ import pandas as pd
 import numpy as np
 import re
 
-Care_Tagset = ['JJ','JJR','JJS','MD','NN','NNS','NNP','NNPS','RB','RBR','RBS',
-               'VB','VBD','VBG','VBN','VBP','VBZ']
-Harvard_Selected = ['Positiv','Negativ','Pstv','Ngtv','Strong','Weak','Active',
-                   'Passive','EMOT','Virtue','Vice','Ovrst','Undrst','Role',
-                   'Need','Goal','Try','Means','Persist','Complet','Fail',
-                   'Think','Know','Causal','Ought','Perceiv','Compare','EVAL',
-                   'Solve','ABS']
-LM_Selected = ['Negative','Positive','Uncertainty','Litigious',
-               'Constraining','Superfluous','Interesting',	'Modal']
+Care_Tagset = ['JJ', 'JJR', 'JJS', 'MD', 'NN', 'NNS', 'NNP', 'NNPS', 'RB', 'RBR', 'RBS',
+               'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
+
+Harvard_Selected = ['Positiv', 'Negativ', 'Pstv', 'Ngtv', 'Strong', 'Weak', 'Active',
+                    'Passive', 'EMOT', 'Virtue', 'Vice', 'Ovrst', 'Undrst', 'Role',
+                    'Need', 'Goal', 'Try', 'Means', 'Persist', 'Complet', 'Fail',
+                    'Think', 'Know', 'Causal', 'Ought', 'Perceiv', 'Compare', 'EVAL',
+                    'Solve', 'ABS']
+
+LM_Selected = ['Negative', 'Positive', 'Uncertainty', 'Litigious',
+               'Constraining', 'Superfluous', 'Interesting', 'Modal']
 LM_MasterDict = pd.read_excel('LoughranMcDonald_MasterDictionary_2014.xlsx')
 HV4_INQ = pd.read_excel('inquireraugmented.xls')
-HVred = HV4_INQ.loc[2:,['Entry']].astype(str)
-HVred[Harvard_Selected]=pd.notnull(HV4_INQ.loc[2:,Harvard_Selected])
-LMred = LM_MasterDict.loc[:,['Word']].astype(str)
-LMred[LM_Selected]=np.sign(LM_MasterDict.loc[2:,LM_Selected])
+HVred = HV4_INQ.loc[2:, ['Entry']].astype(str)
+HVred[Harvard_Selected] = pd.notnull(HV4_INQ.loc[2:, Harvard_Selected])
+LMred = LM_MasterDict.loc[:, ['Word']].astype(str)
+LMred[LM_Selected] = np.sign(LM_MasterDict.loc[2:, LM_Selected])
 lLM = len(LM_Selected)
 ltotal = len(Harvard_Selected + LM_Selected)
 
-HVred['Entry'] = HVred['Entry'].replace(r'#.*','',regex=True)
+HVred['Entry'] = HVred['Entry'].replace(r'#.*', '', regex=True)
 HVred = HVred.groupby(['Entry'], as_index=False)[Harvard_Selected].mean()
 
 HVdict = HVred['Entry'].tolist()
 LMdict = LMred['Word'].tolist()
+
 
 def read_bulk(filename):
     lines = []
@@ -44,6 +47,7 @@ def read_bulk(filename):
     blob = ' '.join(lines)
     return blob
 
+
 def get_candidates(blob):
     wnl = nltk.stem.wordnet.WordNetLemmatizer()
     wordlist = []
@@ -52,9 +56,9 @@ def get_candidates(blob):
     token_pospairs = nltk.pos_tag(tokens)
     # negated = sentiment.util.mark_negation(tokens) # not a good function
     for ind, pair in enumerate(token_pospairs):
-        if pair[0]=='not':
+        if pair[0] == 'not':
             prev_not = True
-        elif (pair[1]=='.') or (pair[1]==',') or (pair[1]==':'):
+        elif (pair[1] == '.') or (pair[1] == ',') or (pair[1] == ':'):
             prev_not = False
         elif pair[1] in Care_Tagset:
             stemmed = wnl.lemmatize(pair[0])
@@ -64,14 +68,15 @@ def get_candidates(blob):
             wordlist.append(stemmed.upper())
     return wordlist
 
+
 def find_scores(docwords):
     cumscore = np.zeros((ltotal,), dtype=float)
     for word in docwords:
         if word in LMdict:
             cumscore[0:lLM] += \
-            LMred.query('Word=="%s"' % word).as_matrix(LM_Selected)[0]
+                LMred.query('Word=="%s"' % word).as_matrix(LM_Selected)[0]
         if word in HVdict:
             cumscore[lLM:] += HVred.query(
-                    'Entry=="%s"' % word).as_matrix(Harvard_Selected)[0]
-    
-    return(cumscore)
+                'Entry=="%s"' % word).as_matrix(Harvard_Selected)[0]
+    cumscore = np.array([cumscore])
+    return cumscore
