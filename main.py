aba12794f6dad755163be04b6197691213ad3952
CYUlysses <ulysses.cy@gmail.com>
2017-05-06 22:07:38 -0400

    Added some comments; more functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# from senti_scoring import *
+from senti_scoring import *
 import shutil
 
 datafolder = 'data_nlp'
@@ -11,6 +11,7 @@ os.chdir(datafolder)
 attrlist = ['Tic', 'Date','Type'] + LM_Selected + Harvard_Selected
 entries = np.array(np.zeros((1,len(attrlist))))
 
+writeind = 0
 for fileind, onefile in enumerate(filelist):
     print('Processing file #%s, file name "%s"' % (str(fileind), onefile))
     alltxt = read_bulk(onefile)
@@ -19,13 +20,27 @@ for fileind, onefile in enumerate(filelist):
     ids = np.array([onefile[:-4].split('-')])
     docentry = np.concatenate((ids, docscore), axis=1)
     entries = np.append(entries, docentry, axis=0)
-    if fileind % 10 == 5:
-        shutil.copy2('tempsave.csv','tempreadable.csv')
+    if fileind % 10 == 9:
+        try:
+            shutil.copy2('autosave.csv','tempread.csv')
+        except:
+            pass
         tblsave = pd.DataFrame(entries[1:,:], columns=attrlist)
-        tblsave.to_csv('tempsave.csv', sep=',',
+        tblsave.to_csv('autosave.csv', sep=',',
                       encoding='utf-8', mode='w')
+        print('Autosaved')
+    if fileind % 30 == 29:
+        tblsave.to_csv('batch%s.csv' % str(writeind), sep=',',
+                      encoding='utf-8', mode='w')
+        print('Saved 30 entries to file batch%s.csv' % writeind)
+        tblsave = []
+        writeind += 1
+        entries = np.array(np.zeros((1,len(attrlist))))
+        
+writeind = 0
+alltb = save_combiner()
 os.chdir('..')
-tblout = pd.DataFrame(entries, columns=attrlist)
-tblout.to_csv('NLPed.csv', sep=',', encoding='utf-8', mode='w')
+# tblout = pd.DataFrame(entries, columns=attrlist)
+alltb.to_csv('NLPcombined.csv', sep=',', encoding='utf-8', mode='w')
 
 print('The end is the beginning is the end')
--- senti_scoring.py
+++ senti_scoring.py
@@ -12,20 +12,29 @@ import pandas as pd
 import numpy as np
 import re
 
+# Only words of these kinds bear sentimental implications
 Care_Tagset = ['JJ', 'JJR', 'JJS', 'MD', 'NN', 'NNS', 'NNP', 'NNPS', 'RB', 'RBR', 'RBS',
                'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ']
-
+# Hand-selected word sentiment categories from Harvard Inquirer Dictionary
+# that are related to the financial report.
 Harvard_Selected = ['Positiv', 'Negativ', 'Pstv', 'Ngtv', 'Strong', 'Weak', 'Active',
                     'Passive', 'EMOT', 'Virtue', 'Vice', 'Ovrst', 'Undrst', 'Role',
                     'Need', 'Goal', 'Try', 'Means', 'Persist', 'Complet', 'Fail',
                     'Think', 'Know', 'Causal', 'Ought', 'Perceiv', 'Compare', 'EVAL',
                     'Solve', 'ABS']
-
+# Categories by Loughran and McDonald
 LM_Selected = ['Negative', 'Positive', 'Uncertainty', 'Litigious',
                'Constraining', 'Superfluous', 'Interesting', 'Modal']
+# Read LM Dictionary
 LM_MasterDict = pd.read_excel('LoughranMcDonald_MasterDictionary_2014.xlsx')
+# Read HID Dictionary
 HV4_INQ = pd.read_excel('inquireraugmented.xls')
+# The word list contains the word "FALSE" which might be automatically
+# converted to boolean; forcing the original string format.
+# Construct the first index column
 HVred = HV4_INQ.loc[2:, ['Entry']].astype(str)
+# Read data; convert words in original dataset into boolean values for easier
+# calculation and manipulation
 HVred[Harvard_Selected] = pd.notnull(HV4_INQ.loc[2:, Harvard_Selected])
 LMred = LM_MasterDict.loc[:, ['Word']].astype(str)
 LMred[LM_Selected] = np.sign(LM_MasterDict.loc[2:, LM_Selected])
@@ -33,13 +42,18 @@ lLM = len(LM_Selected)
 ltotal = len(Harvard_Selected + LM_Selected)
 
 HVred['Entry'] = HVred['Entry'].replace(r'#.*', '', regex=True)
+# For same word with multiple tenses, simply use the averaged value
 HVred = HVred.groupby(['Entry'], as_index=False)[Harvard_Selected].mean()
 
+# Word lists for faster lookup
 HVdict = HVred['Entry'].tolist()
 LMdict = LMred['Word'].tolist()
 
 
 def read_bulk(filename):
+    '''
+    Get.All.Lines.
+    '''
     lines = []
     with open(filename, 'r', encoding='utf-8') as infile:
         for line in infile:
@@ -49,8 +63,14 @@ def read_bulk(filename):
 
 
 def get_candidates(blob):
+    '''
+    Parse words and find those worth inspecting
+    '''
     wnl = nltk.stem.wordnet.WordNetLemmatizer()
     wordlist = []
+    # prev_not is the custom negation indicator
+    # has effect only on the nearest verb/adjective
+    # switches on by parsed "not", reset on effect or termination symbols
     prev_not = False
     tokens = nltk.tokenize.word_tokenize(blob)
     token_pospairs = nltk.pos_tag(tokens)
@@ -60,8 +80,10 @@ def get_candidates(blob):
             prev_not = True
         elif (pair[1] == '.') or (pair[1] == ',') or (pair[1] == ':'):
             prev_not = False
+        # Find the word stem using the Word Net Lemmatizer in NLTK package
         elif pair[1] in Care_Tagset:
             stemmed = wnl.lemmatize(pair[0])
+            # Negation labeling
             if prev_not:
                 stemmed = 'not_' + stemmed
                 prev_not = False
@@ -70,13 +92,55 @@ def get_candidates(blob):
 
 
 def find_scores(docwords):
+    '''
+    Find a documents sentiment scores based on parsed words.
+    return: 1-d numpy array
+    '''
     cumscore = np.zeros((ltotal,), dtype=float)
     for word in docwords:
+        # Basically a LUT process
+        if 'NOT_' in word:
+            word = word[4:]
+            pola = -1
+        else:
+            pola = 1
+            
         if word in LMdict:
-            cumscore[0:lLM] += \
+            cumscore[0:lLM] += pola * \
                 LMred.query('Word=="%s"' % word).as_matrix(LM_Selected)[0]
         if word in HVdict:
-            cumscore[lLM:] += HVred.query(
+            cumscore[lLM:] += pola * HVred.query(
                 'Entry=="%s"' % word).as_matrix(Harvard_Selected)[0]
+
     cumscore = np.array([cumscore])
     return cumscore
+
+def extend_digits(num_digits):
+    formstr = 'batch{0:0>' + str(num_digits) + '}.csv'
+    for filename in os.listdir():
+        if filename.startswith('batch'):
+            numlist = re.findall(r'h(\d+)\.csv', filename)
+            if numlist:
+                newname = formstr.format(int(numlist[0]))
+
+def save_combiner():
+    filelist = os.listdir()
+    filelist.sort()
+    tblist = []
+    for filename in filelist:
+        if 'batch' in filename:
+            tblist.append(pd.read_csv(filename))
+    mastertb = pd.concat(tblist, ignore_index=True)
+    return mastertb
+
+def pre_analysis(tablein):
+    
+
+
+
+
+
+
+
+
+
